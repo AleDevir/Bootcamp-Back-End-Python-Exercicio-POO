e450ca26a82705795c1a298589ee2e6f517e6009
Daiany Morais <141277776+DayMorais@users.noreply.github.com>
2024-08-11 07:44:46 -0300

    Update autor.py

--- src/model/autor.py
+++ src/model/autor.py
@@ -1,3 +1,17 @@
 '''
-Classe Autor
+classe Autor
 '''
+from src.model.base import Base
+
+class Autor(Base):
+
+    def __init__(
+            self,
+            nome: str,
+            identificacao: int = 0
+    ) -> None:
+        super().__init__(identificacao)
+        self.nome: str = nome
+
+    def __str__(self) -> str:
+        return self.nome
