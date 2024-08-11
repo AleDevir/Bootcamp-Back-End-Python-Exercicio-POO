ddc8cf444b22842ab5c152fde89b248d4e5733e5
Rafaela <rafaela.clep@yahoo.com.br>
2024-08-10 23:19:21 -0300

    Update livro_renovavel.py
    update code livro renovavel

--- src/model/livro_renovavel.py
+++ src/model/livro_renovavel.py
@@ -1,5 +1,7 @@
-# Modelo Biblioteca
-# Livro renovavel
+
+# Gerenciamento de Biblioteca
+# Livro Renovavel
+
 
 from src.model.base import Base
 from src.model.emprestimo import Emprestimo
@@ -7,8 +9,8 @@ from src.model.usuario import Usuario
 from src.model.livro import Livro
 from src.model.exemplar import Exemplar
 
-def renovacoes_permitidas
-
 class LivroRenovavel(Livro):
-    renovar_emprestimo_exemplar(exemplar: Exemplar) = None
+
+    def __init__(renovacoes_permitidas):
+        self.renovacoes_permitidas = renovacoes_permitidas
 
