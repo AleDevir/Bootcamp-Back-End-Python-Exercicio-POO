a5a41334965f9cf4f612a87e37cad143cdda49b2
Rafaela <rafaela.clep@yahoo.com.br>
2024-08-11 11:13:40 -0300

    Update genero.py
    corrigindo a classe genero

--- src/model/genero.py
+++ src/model/genero.py
@@ -1,3 +1,10 @@
-'''
-Classe Genero
-'''
+#classe genero
+
+from src.model.base import Base
+
+class Genero(Base):
+    def __init__(self, nome:str, identificacao: int = 0):
+    # Chama o construtor da classe base com o parâmetro identificacao
+        super().__init__(identificacao)
+        # Atribui o nome ao atributo da instância
+        self.nome = nome
